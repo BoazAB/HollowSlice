b53c5f9f69c43a22c0f71f5db0bfa796770a2e21
Blauwberry <32714@ma-web.nl>
2022-11-23 11:37:19 +0100

    left and right attack

--- Assets/Scripts/SwingNail.cs
+++ Assets/Scripts/SwingNail.cs
@@ -5,14 +5,45 @@ using UnityEngine;
 
 public class SwingNail : MonoBehaviour
 {
+    private bool facingRight;
+    public GameObject hitLine;
+    private Vector3 rightPlace;
+
     void Update()
     {
         if (Input.GetKeyDown("x"))
         {
+            swing();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            facingRight = true;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            facingRight = false;
+        }
+    }
+    private void swing()
+    {
+        if (facingRight)
+        {
+            rightPlace = new Vector3(this.gameObject.transform.position.x + 1f, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+            var newHit = Instantiate(hitLine, rightPlace, Quaternion.identity);
+            newHit.transform.parent = this.gameObject.transform;
+            Destroy(newHit, 1);
 
+        }
+        else if (!facingRight)
+        {
+            rightPlace = new Vector3(this.gameObject.transform.position.x + -1f, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+            var newHit = Instantiate(hitLine, rightPlace, Quaternion.identity);
+            newHit.transform.parent = this.gameObject.transform;
+            Destroy(newHit, 1);
         }
     }
 }
+
 
 //register if attack button is hit
 //get the direction youre facing
