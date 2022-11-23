60da235d4ae7f24ae5a2ee3547b769b8a9414a29
Blauwberry <32714@ma-web.nl>
2022-11-23 09:32:17 +0100

    Update

---
+++ Assets/Scripts/SwingNail.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SwingNail : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("x"))
+        {
+
+        }
+    }
+}
+
+//register if attack button is hit
+//get the direction youre facing
+//spawn hitbox in that direction
