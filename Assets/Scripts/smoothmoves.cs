4f0593f2c40522a8a7a0c7510519f88a7e92a6c8
Mauro Hendriks <89855665+Mauro-Hendriks@users.noreply.github.com>
2022-11-23 12:50:50 +0100

    added ui elements (not ui objects jet), geo spin, go left and right

---
+++ Assets/Scripts/smoothmoves.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoothmoves : MonoBehaviour
+{
+    [Header("movement var")]
+    [SerializeField] private int speed;
+    [SerializeField] private int jumpheigt;
+
+    private Rigidbody2D playerRigidbody;
+    
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRigidbody.velocity = new Vector3(-speed, 0) * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerRigidbody.velocity = new Vector3(speed, 0) * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+    }
+}
