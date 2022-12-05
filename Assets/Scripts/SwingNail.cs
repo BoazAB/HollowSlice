7ba631e4b6a41ab49622b0369fcec5205fff47bb
Mauro Hendriks <89855665+Mauro-Hendriks@users.noreply.github.com>
2022-12-05 11:37:49 +0100

    test

--- Assets/Scripts/SwingNail.cs
+++ Assets/Scripts/SwingNail.cs
@@ -2,27 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SwingNail : MonoBehaviour
 {
     private bool facingRight;
     public GameObject hitLine;
-    public GameObject hitUp;
     private Vector3 rightPlace;
-    [SerializeField]
-    private bool up;
 
-    [SerializeField]
-    private bool attacking;
-    private void Start()
-    {
-    }
+    
+    public bool attacking;
+
     void Update()
     {
         if (Input.GetKeyDown("x") && attacking == false)
         {
+            
             attacking = true;
+            
             swing();
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
@@ -33,31 +29,26 @@ public class SwingNail : MonoBehaviour
         {
             facingRight = false;
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            up = true;
-        }
-        else
-        {
-            up = false;
-        }
     }
     private void swing()
     {
-        if (up == true)
-        {
-            rightPlace = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + 1, this.gameObject.transform.rotation.z);
-            var newHit = Instantiate(hitUp, rightPlace, Quaternion.identity);
-            Destroy(newHit, 1);
-            StartCoroutine(waitToHit());
-        }
-        else if (facingRight)
+        if (facingRight)
         {
             rightPlace = new Vector3(this.gameObject.transform.position.x + 1f, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
-            var newHit = Instantiate(hitLine, rightPlace, Quaternion.identity.normalized);
+            var newHit = Instantiate(hitLine, rightPlace, Quaternion.identity);
             newHit.transform.parent = this.gameObject.transform;
             Destroy(newHit, 1);
             StartCoroutine(waitToHit());
+            
+        }
+        else if (!facingRight)
+        {
+            rightPlace = new Vector3(this.gameObject.transform.position.x + -1f, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+            var newHit = Instantiate(hitLine, rightPlace, Quaternion.identity);
+            newHit.transform.parent = this.gameObject.transform;
+            Destroy(newHit, 1);
+            StartCoroutine(waitToHit());
+            
         }
     }
     IEnumerator waitToHit()
