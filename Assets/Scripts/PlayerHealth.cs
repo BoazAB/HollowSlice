d01215f9df301dc5c8cfe21ea6ab804c81c4369a
Blauwberry <32714@ma-web.nl>
2022-12-19 10:07:30 +0100

    helth(not done)

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int HP;
+    public int HP = 9;
+    public Text HealthText;
     public int NumMasks;
 
     public Image[] Masks;
@@ -15,13 +16,16 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        HP = 9;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HealthText.text = HP.ToString();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HP--;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
