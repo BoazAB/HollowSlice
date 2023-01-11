34a7166f50084cc698b3341b5bc2bab92a4da095
JobHeilig <jobheilig@gmail.com>
2023-01-11 12:02:27 +0100

    Spear enemy heeft een nieuwe sprite en movement script
    De spear Enemy heeft een nieuwe sprite en movement script. Nu zonder waypoints. De spearEnemy kijkt nu ook altijd de player aan

---
+++ Assets/Scripts/spearScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 
+ * DIT IS HET OUDE SPEAR SCRIPT.
+ * 
+public class spearScript : MonoBehaviour
+{
+
+    public GameObject spear;
+    public Transform spearPos;
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 2)
+        {
+            timer = 0;
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(spear, spearPos.position, Quaternion.identity);
+    }
+}
+
+*/
